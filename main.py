903a21084b8fd62d8676d0dd1b8059538067c7cf
anidas <anirbandas6731@gmail.com>
2026-04-19 12:44:50 +0530

    fixing

--- main.py
+++ main.py
@@ -161,7 +161,7 @@ def chat(request: Request, prompt: str, model: str = "dagr"):
                 "fallback": "meta-llama/llama-3.3-70b-instruct:free",
             },
             "qwen": {
-                "primary": "nvidia/nemotron-3-super-120b-a12b:free",
+                "primary": "z-ai/glm-4.5-air:free",
                 # "fallback": "openai/gpt-oss-120b:free",
             },
         }
